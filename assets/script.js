b4696bf0a2f48319aadcf388a72aa5aca1114580
Danilopc <sr_danilo@hotmail.com>
2024-01-14 19:51:10 +0000

    current temp added

--- assets/script.js
+++ assets/script.js
@@ -1,8 +1,17 @@
 
+
+
+
+
+
+
+
+
 $('#search-button').on("click", function (event) {
     var city = $('#search-input').val()
     event.preventDefault()
     getCoordinates(city)
+
 
 })
 
@@ -16,7 +25,7 @@ function getCoordinates(city) {
             lat = data[0].lat;
             lon = data[0].lon;
 
-            getWeather(lat,lon)
+            getWeather(lat, lon)
         });
 
 }
@@ -25,21 +34,51 @@ function getWeather(lat, lon) {
 
     var getFiveDays = `http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=059158fd829c06a2a0fee4443308c7ba`
 
+    getMainTemp(lat, lon)
     fetch(getFiveDays)
         .then(function (response) {
             return response.json();
         })
         .then(function (data) {
-            console.log(data)
-            console.log(data.list[0].main.temp)
+
         });
 }
 
+function getMainTemp(lat, lon) {
+    var api = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=059158fd829c06a2a0fee4443308c7ba`
 
-// get the current weather
-//next five days weather 
+    fetch(api)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            var currentTemp = (data.main.temp + (-273.15)).toFixed(1)
+            cityData = [data.name, data.weather[0].icon, data.main.temp, data.wind.speed, data.main.humidity,]
+            console.log(cityData)
 
-// create a main card to display the current weather
-//   -- city name + date temp wind and humidity
-//create 5 mini cards to display the next five days 
-//  --each card contain city name, date, temp,wind and humidity
+
+            TodayEl(cityData)
+        });
+
+}
+
+
+function TodayEl(data) {
+    var cityName = data[0];
+    var iconCode = data[1];
+    var todayTemp = data[2] +( -273.15)
+    console.log(todayTemp)
+    var iconUrl = `https://openweathermap.org/img/wn/${iconCode}@2x.png`
+    console.log(cityName)
+    var date = dayjs().format('DD/MMM/YYYY');
+    // var icon = $('<img)').attr('src', iconUrl)
+
+     $('#city-date').text(cityName + " " + date )
+    $('#temp').text("Temperature: " +  todayTemp.toFixed(1) + " C°")
+    $('#wind').text("Wind speed: " +   data[3] + "Km")
+    $('#humidity').text("Humidity: " +data[4])
+}
+
+//next five days weather
+
+//populate the cards
